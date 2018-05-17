8f575bda4bdd83e720815f009478ae7adbe3fa4a
walkert <walkert20@hanover.edu>
2018-05-17 13:33:52 -0400

    DelayedMessageService completed.

--- app/src/main/java/edu/hanover/joke/DelayedMessageService.java
+++ app/src/main/java/edu/hanover/joke/DelayedMessageService.java
@@ -2,14 +2,23 @@ package edu.hanover.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
 import android.util.Log;
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -27,5 +36,11 @@ public class DelayedMessageService extends IntentService {
 
     private void showText(final String text) {
         Log.v("DelayedMessageService", "The message is: " + text);
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
