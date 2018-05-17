1e4864a3936dba226197a830b14fb68e166afaf0
walkert <walkert20@hanover.edu>
2018-05-17 13:21:36 -0400

    added button to xml.

--- app/src/main/java/edu/hanover/joke/MainActivity.java
+++ app/src/main/java/edu/hanover/joke/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.hanover.joke;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
