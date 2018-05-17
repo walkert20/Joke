ac2074f281cca96742e2fac442a18c177e3d4a00
walkert <walkert20@hanover.edu>
2018-05-17 13:12:25 -0400

    Wrote delayedMessageserice file.

--- app/src/main/java/edu/hanover/joke/DelayedMessageService.java
+++ app/src/main/java/edu/hanover/joke/DelayedMessageService.java
@@ -2,9 +2,11 @@ package edu.hanover.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
-
+import android.util.Log;
 
 public class DelayedMessageService extends IntentService {
+
+    public static final String EXTRA_MESSAGE = "message";
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -12,5 +14,18 @@ public class DelayedMessageService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        synchronized (this) {
+            try {
+                wait(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        showText(text);
+    }
+
+    private void showText(final String text) {
+        Log.v("DelayedMessageService", "The message is: " + text);
     }
 }
